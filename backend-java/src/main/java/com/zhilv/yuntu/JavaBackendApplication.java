f02677d45e56618e92a5ecb1194ee4aa97f5bf5a
uly609 <78928321+uly609@users.noreply.github.com>
2026-07-09 07:10:36 +0800

    Add Java backend application entry

---
+++ backend-java/src/main/java/com/zhilv/yuntu/JavaBackendApplication.java
@@ -0,0 +1,11 @@
+package com.zhilv.yuntu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBackendApplication.class, args);
+    }
+}
